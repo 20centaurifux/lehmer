94dcbc9dd1352db0b329cfbd4fe7d58ea15b589e
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2023-11-21 18:53:48 +0100

    documentation

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
   :license {:name "AGPLv3"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/test.check "1.1.1"]
-                 [org.clojure/core.match "1.0.1"]]
-  :main ^:skip-aot lehmer.core
+                 [org.clojure/test.check "1.1.1"]]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot :all}
+             :test {:dependencies [[org.clojure/core.match "1.0.1"]]}}
+  :plugins [[lein-marginalia "0.9.1"]])
